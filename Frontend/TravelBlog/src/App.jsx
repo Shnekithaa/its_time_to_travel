b405a89b75abc157ebf3239c47efb68035792ad7
BALASHNEKITHAA SATHEESHKUMAR <shnekithaasatheesh2004@gmail.com>
2024-03-22 09:15:20 +0530

    Navbar added

--- Frontend/TravelBlog/src/App.jsx
+++ Frontend/TravelBlog/src/App.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import Navbar from "./components/Navbar/index"
-import Home from "./components/Home"
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/index";
+import Home from "./components/Home";
+import Places from "./components/Places"
+import Gallery from "./components/Gallery"
+import Favourites from "./components/Favourites"
+import Contact from "./components/Contact"
+import Login from "./components/Login"
 
-import "./App.css"
+import "./App.css";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/places" element={<Places />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- Frontend/TravelBlog/src/components/Navbar/index.jsx
+++ Frontend/TravelBlog/src/components/Navbar/index.jsx
@@ -1,27 +1,42 @@
-import React, { Component } from 'react';
-import "./index.css"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 import { CgProfile } from "react-icons/cg";
 import { FaSearch } from "react-icons/fa";
 
-import travelblogLogo from "../../assets/travelblogLogo.png"
+import travelblogLogo from "../../assets/travelblogLogo.png";
 
 export default class index extends Component {
   render() {
     return (
-      <div className='navbar'>
+      <div className="navbar">
         <div>
-            <img src={travelblogLogo} alt='travel-logo' className='travel_blog_logo' />
+          <img
+            src={travelblogLogo}
+            alt="travel-logo"
+            className="travel_blog_logo"
+          />
         </div>
-        <div className='nav-content'>
+        <div className="nav-content">
+          <Link to="/">
             <div>Home</div>
+          </Link>
+          <Link to="/places">
             <div>Places</div>
+          </Link>
+          <Link to="/gallery">
             <div>Gallery</div>
+          </Link>
+          <Link to="/favourites">
             <div>Favourites</div>
+          </Link>
+          <Link to="/contact">
             <div>Contact</div>
+          </Link>
         </div>
         <div>
-            <CgProfile className='profile-icon' />
-            <FaSearch className='search-icon' />
+          <CgProfile className="profile-icon" />
+          <FaSearch className="search-icon" />
         </div>
       </div>
     );
