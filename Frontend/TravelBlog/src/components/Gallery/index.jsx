fed73727204813fa63bee919b5915645679e52b9
Balashnekithaa S <142475027+Shnekithaa@users.noreply.github.com>
2024-04-03 14:04:24 +0530

    Gallery ui done

--- Frontend/TravelBlog/src/components/Gallery/index.jsx
+++ Frontend/TravelBlog/src/components/Gallery/index.jsx
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import TajMahal from "../../assets/TajMahal.webp"
+import Navbar from "../Navbar";
+
+import './index.css'
+
+const galleryLettersArr = ["G", "A", "L", "L", "E", "R", "Y"];
 
 export default class index extends Component {
   render() {
     return (
       <div>
-        
+        <Navbar />
+        <div className="gallery-title-holder">
+          {galleryLettersArr.map((eachChar) => {
+            return <div className="gallery-title-box">{eachChar}</div>;
+          })}
+        </div>
+        <div className="gallery-img-card">
+          <img src={TajMahal} alt="gallery-img" className="gallery-img" />
+        </div>
       </div>
     );
   }
