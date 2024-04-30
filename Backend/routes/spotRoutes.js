997ceafa34837599a7d49d8c9039594d3da67983
Balashnekithaa S <142475027+Shnekithaa@users.noreply.github.com>
2024-04-30 19:18:48 +0530

    Added update functionality

--- Backend/routes/spotRoutes.js
+++ Backend/routes/spotRoutes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const Joi = require('joi')
 const router = express.Router()
 const placesModel = require("../models/Places")
+const PlacesModel = require("../models/Places")
 
 router.get("/places", (req, res) => {
     placesModel.find()
@@ -21,6 +22,30 @@ router.post("/addPlace", (req, res) => {
     }
     placesModel.create(req.body)
     .then(eachPlace => res.status(201).json(eachPlace))
+    .catch(err => res.json(err))
+})
+
+router.post("/getPlace", (req, res) => {
+    const {spotId} = req.body
+
+    PlacesModel.findById({_id: spotId})
+    .then(spot => res.json(spot))
+    .catch(err => res.json(err))
+})
+
+const updatePlaceSchema = Joi.object({
+    spot: Joi.string().required(),
+    location: Joi.string().required()
+})
+
+router.put("/updatePlace", (req, res) => {
+    const {spotId, spot, location} = req.body
+    const {error} = updatePlaceSchema.validate({spot, location})
+    if(error){
+        return res.status(400).send(error.details[0].message)
+    }
+    placesModel.findByIdAndUpdate({_id: req.body.spotId}, {spot: req.body.spot, location: req.body.location}, { new: true })
+    .then(res => res.json(res))
     .catch(err => res.json(err))
 })
 
--- Frontend/TravelBlog/src/components/Places/index.jsx
+++ Frontend/TravelBlog/src/components/Places/index.jsx
@@ -1,57 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from "../Navbar"
-import axios from "axios"
-import eiffel_tower from "../../assets/eiffel_tower.jpg"
+import React, { useEffect, useState } from "react";
+import Navbar from "../Navbar";
+import axios from "axios";
+import eiffel_tower from "../../assets/eiffel_tower.jpg";
 import { IoMdAddCircle } from "react-icons/io";
-import AddPlaceModal from "../AddPlaceModal"
-import "./index.css"
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+import AddPlaceModal from "../AddPlaceModal";
+import UpdatePlaceModal from "../UpdatePlaceModal";
+import "./index.css";
 
 const profileLettersArr = ["P", "L", "A", "C", "E", "S"];
 
 const index = () => {
-  const [spots, setSpots] = useState([])
-  const [openAddPlaceModal, setOpenAddPlaceModal] = useState(false)
+  const [spots, setSpots] = useState([]);
+  const [openAddPlaceModal, setOpenAddPlaceModal] = useState(false);
+  const [editSpotId, setEditSpotId] = useState(null);
 
+  const handleEditClick = (id) => {
+    setEditSpotId(id);
+  };
+
+  const handleCloseEditModal = () => {
+    setEditSpotId(null);
+  };
 
   useEffect(() => {
-    axios.get("http://localhost:3000/places")
-    .then((response) => {
-      setSpots(response.data)
-    })
-    .catch(err => console.log(err))
-  }, [])
-
+    axios
+      .get("http://localhost:3000/places")
+      .then((response) => {
+        setSpots(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
-      {console.log(spots)}
       <Navbar />
-      <div className='places-top'>
-        <div className='profile-title-holder'>
-          {
-              profileLettersArr.map((eachLetter) => {
-                  return <div className='profile-letters'>{eachLetter}</div>
-              })
-          }
+      <div className="places-top">
+        <div className="profile-title-holder">
+          {profileLettersArr.map((eachLetter) => {
+            return <div className="profile-letters">{eachLetter}</div>;
+          })}
         </div>
-        <IoMdAddCircle className='add-icon' onClick={() => setOpenAddPlaceModal(true)} />
-        {openAddPlaceModal && <AddPlaceModal setOpenAddPlaceModal={setOpenAddPlaceModal} />}
+        <IoMdAddCircle
+          className="add-icon"
+          onClick={() => setOpenAddPlaceModal(true)}
+        />
+        {openAddPlaceModal && (
+          <AddPlaceModal setOpenAddPlaceModal={setOpenAddPlaceModal} />
+        )}
       </div>
-      <div className='places-holder'>
-        {
-          spots.map((eachSpot, index) => (
-            <div className='spot-img-card' key={index}>
-              <img src={eiffel_tower} alt="gallery-img" className="gallery-img" />
-              <div className='spot-info'>
-                <div>{eachSpot.spot}</div>
-                <div>{eachSpot.location}</div>
-              </div>
+      <div className="places-holder">
+        {spots.map((eachSpot) => (
+          <div className="spot-img-card" key={eachSpot._id}>
+            <img src={eiffel_tower} alt="gallery-img" className="gallery-img" />
+            <div className="spot-info">
+              <div>{eachSpot.spot}</div>
+              <div>{eachSpot.location}</div>
             </div>
-          ))
-        }
+            <div className="icon-holder">
+              <FaRegEdit
+                className="edit-place-icon"
+                onClick={() => handleEditClick(eachSpot._id)}
+              />
+              <MdDeleteOutline className="delete-place-icon" />
+            </div>
+            {editSpotId === eachSpot._id && (
+              <UpdatePlaceModal
+                setOpenEditPlaceModal={handleCloseEditModal}
+                spotId={eachSpot._id}
+              />
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default index;
